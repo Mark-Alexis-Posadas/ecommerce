fb7b35081fdc35a9f748cb8f2f4a3a87db59f954
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-12-02 18:38:14 +0800

    added active state in navbart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,30 @@
 import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import { NavbarProps } from "../types/navbar";
 
 export const Navbar: FC<NavbarProps> = ({ navbarData }) => {
+  const [active, setActive] = useState(0);
+  const handleClick = (index) => {
+    setActive(index);
+  };
   return (
     <nav className="flex items-center p-4 justify-between">
       <h1>logo</h1>
       <ul className="flex items-center gap-4">
-        {navbarData.map((item) => (
+        {navbarData.map((item, index) => (
           <li key={uuidv4()}>
-            <Link to={`/${item.url}`}>{item.name}</Link>
+            <Link
+              to={`/${item.url}`}
+              className={`${
+                active === index ? "text-green-600" : "text-gray-600"
+              }`}
+              onClick={() => handleClick(index)}
+            >
+              {item.name}
+            </Link>
           </li>
         ))}
       </ul>
