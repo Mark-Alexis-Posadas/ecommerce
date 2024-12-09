f578f8d188cde48fbe91ff8371eb4ce4cc72c332
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-12-09 17:27:28 +0800

    added add to cart component in product details

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -3,6 +3,8 @@ import { Section } from "../components/Section";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 import { Product } from "../types/product";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const ProductDetails: FC = () => {
   const { id } = useParams();
@@ -19,6 +21,19 @@ export const ProductDetails: FC = () => {
           <span className="text-gray-600 uppercase text-sm">
             categories: {data.category}
           </span>
+
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4">
+              <button>
+                <FontAwesomeIcon icon={faMinus} />
+              </button>
+              <input type="number" value={1} />
+              <button>
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
+            </div>
+            <button>Add to cart</button>
+          </div>
         </div>
       </div>
     </Section>
